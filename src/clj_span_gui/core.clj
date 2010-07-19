8e95a3ce28c17602ad5c2663d6b8f1238b5ba62d
Gary Johnson <gwjohnso@uvm.edu>
2010-07-19 08:33:15 +0800

    Cleaned up syntax of core.clj

--- src/clj_span_gui/core.clj
+++ src/clj_span_gui/core.clj
@@ -1,130 +1,119 @@
 (ns clj-span-gui.core
   (:import (javax.swing JFrame JLabel JTextField JButton ButtonGroup JRadioButton)
-	   (java.awt.event ActionListener)
-	   (java.awt GridLayout)))
+           (java.awt.event ActionListener)
+           (java.awt GridLayout)))
 
 (defn init []
-  (let [frame (new JFrame "clj-span-gui")
-	source-layer-text (new JTextField)
-	sink-layer-text (new JTextField)
-	use-layer-text (new JTextField)
-	flow-layer-text (new JTextField)
-	source-layer-label (new JLabel "Source Layer")
-	sink-layer-label (new JLabel "Sink Layer")
-	use-layer-label (new JLabel "Use Layer")
-	flow-layer-label (new JLabel "Flow Layer")
+  "Constructs a JFrame containing numerous widgets for controlling the
+   SPAN system.  Returns the JFrame object."
+  (let [source-layer-text  (JTextField.)
+        sink-layer-text    (JTextField.)
+        use-layer-text     (JTextField.)
+        flow-layer-text    (JTextField.)
+        source-layer-label (JLabel. "Source Layer")
+        sink-layer-label   (JLabel. "Sink Layer")
+        use-layer-label    (JLabel. "Use Layer")
+        flow-layer-label   (JLabel. "Flow Layer")
 	
-	source-thresh-text (new JTextField)
-	sink-thresh-text (new JTextField)
-	use-thresh-text (new JTextField)
-	trans-thresh-text (new JTextField)
-	source-thresh-label (new JLabel "Source Threshold")
-	sink-thresh-label (new JLabel "Sink Threshold")
-	use-thresh-label (new JLabel "Use Threshold")
-	trans-thresh-label (new JLabel "Trans Threshold")
+        source-thresh-text  (JTextField.)
+        sink-thresh-text    (JTextField.)
+        use-thresh-text     (JTextField.)
+        trans-thresh-text   (JTextField.)
+        source-thresh-label (JLabel. "Source Threshold")
+        sink-thresh-label   (JLabel. "Sink Threshold")
+        use-thresh-label    (JLabel. "Use Threshold")
+        trans-thresh-label  (JLabel. "Trans Threshold")
 	
-	rv-max-states-text (new JTextField)
-	downscaling-factor-text (new JTextField)
-	rv-max-states-label (new JLabel "RV Max States")
-	downscaling-factor-label (new JLabel "Downscaling Factor")
-
+        rv-max-states-text       (JTextField.)
+        downscaling-factor-text  (JTextField.)
+        rv-max-states-label      (JLabel. "RV Max States")
+        downscaling-factor-label (JLabel. "Downscaling Factor")
 	
-	sink-type-label (new JLabel "Sink Type")
-	sink-type-buttongroup (new ButtonGroup)
-	sink-absolute-button (new JRadioButton "Absolute" false)
-	sink-relative-button (new JRadioButton "Relative" false)
-
-
-
-	use-type-label (new JLabel "Use Type")
-	use-type-buttongroup (new ButtonGroup)
-	use-absolute-button (new JRadioButton "Absolute" false)
-	use-relative-button (new JRadioButton "Relative" false)
-
-
-
-	benefit-type-label (new JLabel "Benefit Type")
-	benefit-type-buttongroup (new ButtonGroup)
-	benefit-rival-button (new JRadioButton "Rival" false)
-	benefit-non-button (new JRadioButton "Non-Rival" false)
-
-
-
-	flow-model-label (new JLabel "Flow Model")
-	flow-model-buttongroup (new ButtonGroup)
-	flow-lineofsight-button (new JRadioButton "Line of Sight" false)
-	flow-proximity-button (new JRadioButton "Proximity" false)
-	flow-carbon-button (new JRadioButton "Carbon" false)
-	flow-sediment-button (new JRadioButton "Sediment" false)
-
-
+        sink-type-label       (JLabel. "Sink Type")
+        sink-absolute-button  (JRadioButton. "Absolute" false)
+        sink-relative-button  (JRadioButton. "Relative" false)
+        sink-type-buttongroup (doto (ButtonGroup.)
+                                (.add sink-absolute-button)
+                                (.add sink-relative-button))
+        
+        use-type-label       (JLabel. "Use Type")
+        use-absolute-button  (JRadioButton. "Absolute" false)
+        use-relative-button  (JRadioButton. "Relative" false)
+        use-type-buttongroup (doto (ButtonGroup.)
+                               (.add use-absolute-button)
+                               (.add use-relative-button))
+        
+        benefit-type-label       (JLabel. "Benefit Type")
+        benefit-rival-button     (JRadioButton. "Rival" false)
+        benefit-non-button       (JRadioButton. "Non-Rival" false)
+        benefit-type-buttongroup (doto (ButtonGroup.)
+                                   (.add benefit-rival-button)
+                                   (.add benefit-non-button))
+        
+        flow-model-label        (JLabel. "Flow Model")
+        flow-lineofsight-button (JRadioButton. "Line of Sight" false)
+        flow-proximity-button   (JRadioButton. "Proximity" false)
+        flow-carbon-button      (JRadioButton. "Carbon" false)
+        flow-sediment-button    (JRadioButton. "Sediment" false)
+        flow-model-buttongroup  (doto (ButtonGroup.)
+                                  (.add flow-lineofsight-button)
+                                  (.add flow-proximity-button)
+                                  (.add flow-carbon-button)
+                                  (.add flow-sediment-button))
 	
-	engage-button (new JButton "Engage")]
+        engage-button (doto (JButton. "Engage")
+                        (.addActionListener
+                         (proxy [ActionListener] []
+                           (actionPerformed [evt]
+                                            (let [c (Double/parseDouble (.getText source-layer-text))]
+                                              (.setText source-layer-label
+                                                        (str (+ 32 (* 1.8 c)) " Fahrenheit")))))))]
 
-  (doto sink-type-buttongroup
-	  (.add sink-absolute-button)
-	  (.add sink-relative-button))
-
-    (doto use-type-buttongroup
-	  (.add use-absolute-button)
-	  (.add use-relative-button))
-	
-    (doto benefit-type-buttongroup
-	  (.add benefit-rival-button)
-	  (.add benefit-non-button))
-
-    (doto flow-model-buttongroup
-	  (.add flow-lineofsight-button)
-	  (.add flow-proximity-button)
-	  (.add flow-carbon-button)
-	  (.add flow-sediment-button))
-	
-
-    (doto frame
+    (doto (JFrame. "clj-span-gui")
       ;;(.setDefaultCloseOperation (JFrame/EXIT_ON_CLOSE))
-      (.setLayout (new GridLayout 2 2 3 3))
-    (.add source-layer-label)
-    (.add source-layer-text)
-    (.add sink-layer-label)
-    (.add sink-layer-text)
-    (.add use-layer-label)
-    (.add use-layer-text)
-    (.add flow-layer-label)
-    (.add flow-layer-text)
-    (.add source-thresh-label)
-    (.add source-thresh-text)
+      (.setLayout (GridLayout. 2 2 3 3))
+      (.add source-layer-label)
+      (.add source-layer-text)
+      (.add sink-layer-label)
+      (.add sink-layer-text)
+      (.add use-layer-label)
+      (.add use-layer-text)
+      (.add flow-layer-label)
+      (.add flow-layer-text)
+      (.add source-thresh-label)
+      (.add source-thresh-text)
 
-    (.add sink-thresh-label)
-    (.add sink-thresh-text)
-    (.add use-thresh-label)
-    (.add use-thresh-text)
-    (.add trans-thresh-label)
-    (.add trans-thresh-text)
+      (.add sink-thresh-label)
+      (.add sink-thresh-text)
+      (.add use-thresh-label)
+      (.add use-thresh-text)
+      (.add trans-thresh-label)
+      (.add trans-thresh-text)
 
-    (.add rv-max-states-label)
-    (.add rv-max-states-text)
-    (.add downscaling-factor-label)
-    (.add downscaling-factor-text)
+      (.add rv-max-states-label)
+      (.add rv-max-states-text)
+      (.add downscaling-factor-label)
+      (.add downscaling-factor-text)
 
-    (.add sink-type-label)
-    (.add sink-absolute-button)
-    (.add sink-relative-button)
+      (.add sink-type-label)
+      (.add sink-absolute-button)
+      (.add sink-relative-button)
     
-    (.add use-type-label)
-    (.add use-absolute-button)
-    (.add use-relative-button)
+      (.add use-type-label)
+      (.add use-absolute-button)
+      (.add use-relative-button)
 
-    (.add benefit-type-label)
-    (.add benefit-rival-button)
-    (.add benefit-non-button)
+      (.add benefit-type-label)
+      (.add benefit-rival-button)
+      (.add benefit-non-button)
 
-    (.add flow-model-label)
-    (.add flow-lineofsight-button)
-    (.add flow-proximity-button)
-    (.add flow-carbon-button)
-    (.add flow-sediment-button)
+      (.add flow-model-label)
+      (.add flow-lineofsight-button)
+      (.add flow-proximity-button)
+      (.add flow-carbon-button)
+      (.add flow-sediment-button)
 
-    (.add engage-button)
+      (.add engage-button)
 
       (.setVisible true))))
 
